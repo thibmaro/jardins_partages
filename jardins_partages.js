6f08fe694009646d25e9f760be2ae2db1262a22b
thibmaro <thibault.maroselli@hotmail.fr>
2019-05-31 13:10:30 +0200

    évolution du projet

--- jardins_partages.js
+++ jardins_partages.js
@@ -1,3 +1,16 @@
-$.getJSON('https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_jardins_partages_a_paris&facet=arrdt&facet=charte', function(result){
-	console.log(result);
+$.getJSON('https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_jardins_partages_a_paris&rows=90&facet=arrdt&facet=charte', function(result){
+	
+	for(var i = 0; i < result.records.length; i++){
+		console.log(result.records[i].fields.adresse);
+		// console.log(result);
+	}
+	// console.log(result.records[0].fields.adresse);
 });
+
+
+// function getResult(result) {
+// 	console.log(result);
+// 	resultat = result;
+// 	return resultat ;
+// }
+// console.log(resultat);
