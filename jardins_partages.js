555af1622a55902af33972be5934b8db7a703310
thibmaro <thibault.maroselli@hotmail.fr>
2019-06-06 13:43:17 +0200

    ajout de la map (absence de marqueurs)

--- jardins_partages.js
+++ jardins_partages.js
@@ -1,16 +1,25 @@
-$.getJSON('https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_jardins_partages_a_paris&rows=90&facet=arrdt&facet=charte', function(result){
-	
+var map;
+var url;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById("myMap"),{
+  	center: {lat: 48.866667, lng: 2.333333},
+  	zoom: 13
+	});
+}
+
+url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_jardins_partages_a_paris&rows=100&sort=-arrdt&facet=arrdt&facet=charte'; 
+$.getJSON(url, function(result){
+	var ul = document.createElement('ul');
 	for(var i = 0; i < result.records.length; i++){
-		console.log(result.records[i].fields.adresse);
-		// console.log(result);
+		var li = document.createElement('li');
+		li.innerHTML = result.records[i].fields.arrdt +'<sup>arr</sup>'+ ' - ' + result.records[i].fields.nom_ev + ' - ' + result.records[i].fields.adresse;
+		ul.appendChild(li);
+
+		
 	}
-	// console.log(result.records[0].fields.adresse);
+	// console.log(result);
+	var main = document.getElementById('main').appendChild(ul);
 });
 
 
-// function getResult(result) {
-// 	console.log(result);
-// 	resultat = result;
-// 	return resultat ;
-// }
-// console.log(resultat);
